709379b7422b58bbecf8162c1ab183e3a1c9cd90
Nathan <nat.kunk@gmail.com>
2025-05-05 16:43:05 +1000

    fixes

--- leaderboard_app.py
+++ leaderboard_app.py
@@ -13,6 +13,8 @@ import re
 
 # Import Selenium libraries for web scraping
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
